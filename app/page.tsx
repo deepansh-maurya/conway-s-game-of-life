3bbb2ef69fa9a4ee4cca91b83b6a0949ee466709
deepansh <mauryadeepansh40@gmail.com>
2024-07-13 22:47:24 +0530

    grid build, logic building started

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,131 @@
-import Image from "next/image";
-
+"use client";
+import { useState } from "react";
 export default function Home() {
+  const rows = 30;
+  const cols = 30;
+  const [grid, setGrid] = useState<number[][]>(
+    Array(rows).fill(Array(cols).fill(0))
+  );
+  const handleEvolution = () => {
+    const tempGrid = grid.map((row) => [...row]);
+    // console.log(tempGrid);
+    tempGrid.forEach((rowVal, row) => {
+      let flag = 1;
+      let rowIndex = row;
+      rowVal.map((colVal, colIndex) => {
+        const currentPos = [rowIndex, colIndex];
+        console.log(rowIndex, flag++, rowVal.length);
+        const topPos = --rowIndex > 0 ? [--rowIndex, colIndex] : null;
+        const downPos = ++rowIndex < rows ? [++rowIndex, colIndex] : null;
+        const leftPos = --colIndex > 0 ? [rowIndex, --colIndex] : null;
+        const rightPos = ++colIndex < colIndex ? [rowIndex, --colIndex] : null;
+        const topLeftPos =
+          --rowIndex > 0 && --colIndex > 0 ? [--rowIndex, --colIndex] : null;
+        const topRightPos =
+          --rowIndex > 0 && ++colIndex < cols ? [--rowIndex, ++colIndex] : null;
+        const downRightPos =
+          ++rowIndex < rows && ++colIndex > cols
+            ? [++rowIndex, ++colIndex]
+            : null;
+        const downLeftPos =
+          ++rowIndex < rows && --colIndex > 0 ? [++rowIndex, --colIndex] : null;
+        console.log(
+          "curr",
+          currentPos,
+          "top",
+          topPos,
+          "down",
+          downPos,
+          "left",
+          leftPos,
+          "right",
+          rightPos,
+          "topl",
+          topLeftPos,
+          "topr",
+          topRightPos,
+          "downl",
+          downLeftPos,
+          "downr",
+          downRightPos
+        );
+
+        const allPosArray = [
+          topPos ? topPos : null,
+          downPos ? downPos : null,
+          leftPos ? leftPos : null,
+          rightPos ? rightPos : null,
+          topLeftPos ? topLeftPos : null,
+          topRightPos ? topRightPos : null,
+          downLeftPos ? downLeftPos : null,
+          downRightPos ? downRightPos : null,
+        ];
+        const filteredPosArray = allPosArray.filter((pos) => pos != null);
+        if (tempGrid[rowIndex][colIndex] == 1) {
+          let survivalChance = 0;
+          filteredPosArray.map((pos) => {
+            tempGrid[pos[0]][pos[1]] == 1 ? survivalChance++ : null;
+          });
+          survivalChance > 3 || survivalChance < 2
+            ? (tempGrid[rowIndex][colIndex] = 0)
+            : null;
+        } else {
+          let birthChance = 0;
+          filteredPosArray.map((pos) => {
+            tempGrid[pos[0]][pos[1]] == 1 ? birthChance++ : null;
+          });
+          birthChance >= 3 ? (tempGrid[rowIndex][colIndex] = 1) : null;
+        }
+      });
+    });
+
+    for (let row = 0; row < rows; row++) {
+      console.log("aa gay");
+    }
+    setGrid(tempGrid);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      <div className="text-black w-[100vw] h-[100vh] flex items-center justify-center bg-slate-600 border border-black  flex-wrap">
+        <main className="w-[90.2%]  bg-slate-400 overflow-scroll ">
+          <section className="w-[600px] h-[600px] flex flex-wrap overflow-auto ">
+            {grid.map((row, rowIndex) => {
+              return row.map((col, colIndex) => {
+                return (
+                  <span
+                    key={`${colIndex}${rowIndex}`}
+                    onClick={() => {
+                      const tempGrid = grid.map((row) => [...row]);
+                      const tempState = tempGrid[rowIndex][colIndex];
+                      tempGrid[rowIndex][colIndex] = Number(!tempState);
+                      // console.log(
+                      //   tempGrid,
+                      //   tempState,
+                      //   tempGrid[rowIndex][colIndex],
+                      //   rowIndex,
+                      //   colIndex
+                      // );
+                      setGrid(tempGrid);
+                      // setTimeout(() => {
+                      //   handleEvolution();
+                      // }, 700);
+                    }}
+                    className={`w-[20px] h-[20px] border  ${
+                      grid[rowIndex][colIndex] == 1
+                        ? "bg-yellow-500"
+                        : "bg-yellow-100"
+                    } border-black flex `}
+                  >
+                    {colIndex}
+                  </span>
+                );
+              });
+            })}
+          </section>
+        </main>
+
+        <button onClick={handleEvolution}>start</button>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
